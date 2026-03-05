6f6a940571207a5c3f3d758b0f0cb2d91e354774
Rimal <127244146+Rimal1424@users.noreply.github.com>
2026-03-06 00:32:42 +0530

    user controller

--- src/app.js
+++ src/app.js
@@ -15,5 +15,13 @@ app.use(express.urlencoded({extended:true , limit:"16kb"}));
 app.use(express.static("public"))
 app.use(cookieParser());
 
+//Routes import
+
+import userRouter from './routes/user.routes.js';
+
+// Route Declaration
+app.use("api/v1/users" , userRouter);
+
+
 
 export{app}
